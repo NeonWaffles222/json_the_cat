e677a8122ab6a779e7a096be01166a4557bfcff8
Noah McMillan <noahm27@gmail.com>
2023-08-05 23:30:11 -0700

    Added moche tests for the function

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,24 +1,17 @@
 const request = require('request');
 
-const getBreed = (breed) => {
+const fetchBreedDescription = (breed, callback) => {
 
   request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
-    if (error) {
-      console.log('Error:', error);
-      return;
-    }
-
     const data = JSON.parse(body);
-
     try {
       data[0].name;
     } catch (error) {
-      console.log(`Breed ${breed} not found`);
+      callback(error);
       return;
     }
-
-    console.log(data[0].description);
+    callback(error, data[0].description);
   });
 };
 
-getBreed(process.argv[2]);
+module.exports = { fetchBreedDescription };
